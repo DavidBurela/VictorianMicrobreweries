553737009dc7845f611e45d02f2f9c7c95b585a8
David Burela <David.Burela@gmail.com>
2012-12-21 16:17:43 +1100

    Changed to blank View/ViewModel

--- VictorianMicrobreweries.RT/App.xaml.cs
+++ VictorianMicrobreweries.RT/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using VictorianMicrobreweries.RT.Views;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
@@ -64,7 +65,7 @@ namespace VictorianMicrobreweries.RT
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                if (!rootFrame.Navigate(typeof(MainPage), args.Arguments))
+                if (!rootFrame.Navigate(typeof(MainView), args.Arguments))
                 {
                     throw new Exception("Failed to create initial page");
                 }
---
+++ VictorianMicrobreweries.RT/Views/MainView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Windows.UI.Xaml.Navigation;
+
+namespace VictorianMicrobreweries.RT.Views
+{
+    public sealed partial class MainView
+    {
+        public MainView()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+    }
+}
